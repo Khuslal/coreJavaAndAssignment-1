ba23f481e6915fbd84da9d2133b9b98af0f42d88
Khuslal Gupta <guptakhuslal@gmail.com>
2026-04-30 23:34:13 +0545

    Print Prime Number Upto Given Range and Total Count

--- corejava/src/looping/PrimeNumRange.java
+++ corejava/src/looping/PrimeNumRange.java
@@ -2,14 +2,15 @@ package looping;
 
 import java.util.Scanner;
 
-public class PrimeNo {
+public class PrimeNumRange {
 	public static void main(String[] args) {
-
 		Scanner scn = new Scanner(System.in);
 		System.out.println("Enter no. to check whether it is prime : ");
 		int num = scn.nextInt();
+		int total_num = 0;
+		do {
 		int count = 0;
-		for (int div = 2; div * div <= num; div++) {
+		for (int div = 2; div*div <= num; div++) {
 			if (num % div == 0) {
 				count++;
 				break;
@@ -17,9 +18,11 @@ public class PrimeNo {
 		}
 
 		if (count == 0) {
-			System.out.println(num + " is prime.");
-		} else {
-			System.out.println(num + " is not prime.");
-		}
+			System.out.println(num);
+			total_num ++;
+		} 
+		num--;
+		}while(num>1);
+		System.out.println("Total number : "+total_num);
 	}
 }
