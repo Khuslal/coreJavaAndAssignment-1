97426286ebf50ba63bb0cd88e869f924500931a2
Khuslal Gupta <guptakhuslal@gmail.com>
2026-05-02 13:36:51 +0545

    Convert ASCII Printable Code into character and non-printable is invisible by default.

---
+++ corejava/src/convertasciitocharacter/ConvertASCIIToCharacter.java
@@ -0,0 +1,20 @@
+package convertasciitocharacter;
+
+import java.util.Scanner;
+
+public class ConvertASCIIToCharacter {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		
+		System.out.println("Enter the ascii number between 0 - 127 : ");
+		int ascii = scn.nextInt();
+		if(ascii >=0 && ascii <128) {
+			char character = (char)ascii;
+			
+			System.out.println(character);
+		}else {
+			System.out.println("Invalid Command !");
+		}
+		
+	}
+}
