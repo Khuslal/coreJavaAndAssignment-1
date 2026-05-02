8163b3ea5a018df0f5a5e45eebae384c3a1b6063
Khuslal Gupta <guptakhuslal@gmail.com>
2026-05-02 09:26:45 +0545

    updated and removed some comment

--- corejava/src/looping/ForTest.java
+++ corejava/src/looping/ForTest.java
@@ -4,14 +4,14 @@ public class ForTest {
 	public static void main(String[] args) {
 
 		//================Even number=============================
-//		int x = 0;
-//		for (int i = 1; i <= 100; i++) {
-//
-//			if (i % 2 == 0) { // System.out.println(i); // SUM of Even number
-//				x += i;
-//			}
-//		}
-//		System.out.println("The sum of even from 1-100 : "+x);
+		int x = 0;
+		for (int i = 1; i <= 100; i++) {
+
+			if (i % 2 == 0) { // System.out.println(i); // SUM of Even number
+				x += i;
+			}
+		}
+		System.out.println("The sum of even number from 1-100 : "+x);
 
 		
 		//================Odd Number Method 1:===============================
@@ -26,18 +26,21 @@ public class ForTest {
 //		System.out.println("The sum of odd from 1-100 : "+n);
 
 		//=================Odd Number Method 2:=================================
-//		for (int i = 1; i <= 100; i += 2) {
-//			// sum of odd number and even no.
-//
-//			// Odd number
+		int sum = 0 ;
+		for (int i = 1; i <= 100; i += 2) {
+			// Odd number
 //			System.out.println(i);
-//		}
+			
+			// sum of odd number and even no.
+			sum += i;
+		}
+		System.out.println("The sum of odd number from 1-100 : "+sum);
 		
 		//==============Sum of all Numbers from 1 to 100====================
-		int x=0;
+		int z = 0;
 		for(int i = 0; i<=100; i++) {
-			x += i;
+			z += i;
 		}
-		System.out.println("The sum of 1-100 : "+x);
+		System.out.println("The sum of 1-100 : "+z);
 	}
 }
