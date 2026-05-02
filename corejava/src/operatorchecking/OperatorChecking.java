2a9477945fa6462ff9827ab78dbe984b28b94e46
Khuslal Gupta <guptakhuslal@gmail.com>
2026-05-02 12:38:43 +0545

    Operator Checking ( == , < , > ) using user prompt.

---
+++ corejava/src/operatorchecking/OperatorChecking.java
@@ -0,0 +1,21 @@
+package operatorchecking;
+
+import java.util.Scanner;
+
+public class OperatorChecking {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Enter first number : ");
+		int num1 = scn.nextInt();
+		
+		System.out.println("Enter second number : ");
+		int num2 = scn.nextInt();
+		if(num1 == num2) {
+			System.out.println(num1+ " = "+num2);
+		}else if(num1 > num2) {
+			System.out.println(num1+" > "+num2);
+		}else if(num1 < num2) {
+			System.out.println(num1+" < "+num2);
+		}
+	}
+}
