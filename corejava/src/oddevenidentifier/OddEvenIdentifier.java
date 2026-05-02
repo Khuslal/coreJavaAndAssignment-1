099b8ca8b8ccf7fff3a51bfa34344f668f90439d
Khuslal Gupta <guptakhuslal@gmail.com>
2026-05-02 13:34:52 +0545

    Odd Even Number Identifier

---
+++ corejava/src/oddevenidentifier/OddEvenIdentifier.java
@@ -0,0 +1,17 @@
+package oddevenidentifier;
+
+import java.util.Scanner;
+
+public class OddEvenIdentifier {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+
+		System.out.println("Enter the number to check odd/even : ");
+		int num = scn.nextInt();
+		if(num%2 == 0) {
+			System.out.println(num+" = Even Number");
+		}else {
+			System.out.println(num+" = Odd Number");
+		}
+	}
+}
