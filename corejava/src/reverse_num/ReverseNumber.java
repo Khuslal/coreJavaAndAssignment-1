820519f97fe56de0291419238196d5526ae82e52
Khuslal Gupta <guptakhuslal@gmail.com>
2026-05-02 08:21:42 +0545

    Count and Reverse the given number using modulo(%)

---
+++ corejava/src/reverse_num/ReverseNumber.java
@@ -0,0 +1,25 @@
+package reverse_num;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Enter the number to reverse: ");
+		int x = scn.nextInt();
+		int temp = x;
+		int count = 0;
+		
+		while(temp%10 != 0 ) {
+			count ++;
+			temp /= 10;
+		}
+		System.out.println("The count of "+x+" : "+count);
+		
+		for(int i = 0; i<count; i++) {
+			int y = x%10;
+			System.out.println(y);
+			x /= 10;
+		}
+	}
+}
