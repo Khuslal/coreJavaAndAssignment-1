79560fa78b55b5dd030dd5dacceb22767f44f8bf
Khuslal Gupta <guptakhuslal@gmail.com>
2026-05-02 14:31:48 +0545

    variable letter changed and no commit here

--- corejava/src/leapyearcalculator/LeapYearCalculator.java
+++ corejava/src/leapyearcalculator/LeapYearCalculator.java
@@ -1,21 +1,20 @@
 package leapyearcalculator;
 
-import java.time.Year;
 import java.util.Scanner;
 
 public class LeapYearCalculator {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		System.out.println("Enter the year to check whether it is a leap year or not : ");
-		int Year = scn.nextInt() ;
-		
-		if(Year > 0) {
-			if((Year % 4 == 0 && Year % 100 != 0) || (Year % 400 == 0) ){
-				System.out.println(Year+" is Leap Year ");
-			}else {
-				System.out.println(Year+ " is not Leap Year");
+		int year = scn.nextInt();
+
+		if (year > 0) {
+			if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+				System.out.println(year + " is Leap Year ");
+			} else {
+				System.out.println(year + " is not Leap Year");
 			}
-		}else {
+		} else {
 			System.out.println("Invalid Command !");
 		}
 		scn.close();
