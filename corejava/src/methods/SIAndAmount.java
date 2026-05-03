73b781fde7e73586f1a36d48ef8d46141c957bdb
Khuslal Gupta <guptakhuslal@gmail.com>
2026-05-03 14:21:22 +0545

    SI and Amount Calculator using user input

--- corejava/src/methods/SIAndAmount.java
+++ corejava/src/methods/SIAndAmount.java
@@ -1,5 +1,7 @@
 package methods;
+
 import java.util.Scanner;
+
 public class SIAndAmount {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
@@ -10,10 +12,20 @@ public class SIAndAmount {
 		System.out.println("Enter Rate: ");
 		int r = scn.nextInt();
 		int si = si(p, t, r);
-		System.out.println("SI : "+si);
+		System.out.println("SI : " + si);
+		
+		// Amount = SI + Principal;
+		int amount = amount(p,si);
+		System.out.println("Total Amount : "+amount);
 	}
+
 	static int si(int p, int t, int r) {
-		int si = (p*t*r)/100;
+		int si = (p * t * r) / 100;
 		return si;
+	}
+
+	static int amount(int p, int si) {
+		int amount = p+si;
+		return amount;
 	}
 }
