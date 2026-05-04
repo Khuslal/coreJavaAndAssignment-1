ea7905ca4700ee46b6045785903af77ab6d03fc0
Khuslal Gupta <guptakhuslal@gmail.com>
2026-05-04 18:40:32 +0545

    Store and Print name of cities using array

---
+++ corejava/src/array/StoreAndPrintNameOfCities.java
@@ -0,0 +1,21 @@
+package array;
+
+import java.util.Scanner;
+
+public class StoreAndPrintNameOfCities {
+	public static void main(String[] args) {
+		String cities[] = new String[6];
+		// Scanner initialized
+		Scanner scn = new Scanner(System.in);
+		// For Loop to store the data
+		for (int i = 0; i < cities.length; i++) {
+			System.out.println("Enter the name of city " + i + " : ");
+			cities[i] = scn.next(); //stores the data inside array
+		}
+
+		// For Each Loop to fetch the data
+		for (String x : cities) {
+			System.out.println(x);
+		}
+	}
+}
