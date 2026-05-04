7fb7523a5957224ba3bf2ffdf24de9539d16b18e
Khuslal Gupta <guptakhuslal@gmail.com>
2026-05-04 18:38:14 +0545

    Array Test

---
+++ corejava/src/array/ArrayDocs.java
@@ -0,0 +1,20 @@
+package array;
+
+public class ArrayDocs {
+//	Array
+	/*
+	 * ==========Array===========
+	 * # Collection of homogeneous data.
+	 * # Having fixed size
+	 * # Data stored on the basis of indices (index starts from 0)
+	 * # Random Access
+	 * # Contains duplicate data
+	 * # Contiguous Memory Allocation
+	 * # Array is object in java
+	 * 
+	 * # Syntax : 
+	 * 
+	 * 		data_type array_name[] = new data_type[size];
+	 */
+	
+}
---
+++ corejava/src/array/ArrayTest.java
@@ -0,0 +1,26 @@
+package array;
+
+import java.util.Scanner;
+
+public class ArrayTest {
+	public static void main(String[] args) {
+		// Store and print age of five students
+		
+		// Create array
+		int age[] = new int [3];
+		// Write data in array
+		Scanner scn = new Scanner(System.in);
+		for(int i = 0; i<age.length; i++) {
+			System.out.println("Enter age : ");
+			age[i] = scn.nextInt();
+		}
+		
+		// Read data from array
+		// System.out.println(age[0]);
+		// Read data using for each loop
+		for(int x : age) {
+			System.out.println("The age is "+x);
+		}
+
+	}
+}
