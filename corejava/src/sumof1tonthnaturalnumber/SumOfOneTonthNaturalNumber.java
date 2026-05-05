596959b97264e48ab497b6cf18d74b1034df3757
Khuslal Gupta <guptakhuslal@gmail.com>
2026-05-05 10:12:34 +0545

    Sum Of One To nTh Natural Numbers

---
+++ corejava/src/sumof1tonthnaturalnumber/SumOfOneTonthNaturalNumber.java
@@ -0,0 +1,16 @@
+package sumof1tonthnaturalnumber;
+
+import java.util.Scanner;
+
+public class SumOfOneTonthNaturalNumber {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Enter the number whose sum you want to calculate from 1:");
+		int n = scn.nextInt();
+		int count = 0;
+		for ( int i = 1; i<=n; i++) {
+			count += i;
+		}
+		System.out.println(count);
+	}
+}
