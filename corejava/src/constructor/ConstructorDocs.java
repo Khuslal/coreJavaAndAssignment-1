147ada570ad333b5d5a1e795a594a860f92d4be7
Khuslal Gupta <guptakhuslal@gmail.com>
2026-05-07 17:57:43 +0545

    Constructor Docs And CopyConstructor Example in real world projects

---
+++ corejava/src/constructor/ConstructorDocs.java
@@ -0,0 +1,25 @@
+package constructor;
+
+public class ConstructorDocs {
+	/*
+	 * ===========Constructor============== # Used to create object in java. #
+	 * Special type of method which has same name as class name. # No return type #
+	 * If there is not any user defined constructor in class , a default constructor
+	 * is created by java JVM in compile time to create object.
+	 * 
+	 * # Two types of constructor:
+	 * 		1. Default Constructor
+	 * 			# Used to create object and set default values to it.
+	 * 		
+	 * 		# Syntax :
+	 * 			class-name(){
+	 * 				// set default values to object
+	 * 			}
+	 * 		2. Parameterized Constructor
+	 * 			# Use to create object and set dynamic values to it.
+	 * 		# Syntax:
+	 * 			class-name(arg1, arg2, ...,argn){
+	 * 				// set dynamic values to object
+	 * 			}
+	 */
+}
---
+++ corejava/src/constructor/CopyConstructor.java
@@ -0,0 +1,50 @@
+package constructor;
+
+class CopyConstructor {
+
+	String name;
+	int age;
+	String faculty;
+
+	// Normal constructor
+	CopyConstructor(String name, int age, String faculty) {
+		this.name = name;
+		this.age = age;
+		this.faculty = faculty;
+	}
+
+	// Copy constructor
+	CopyConstructor(CopyConstructor s) {
+		this.name = s.name;
+		this.age = s.age;
+		this.faculty = s.faculty;
+	}
+
+	void display() {
+		System.out.println(name + " " + age + " " + faculty);
+	}
+
+	public static void main(String[] args) {
+
+		CopyConstructor s1 = new CopyConstructor("Ram", 20, "Science and Technology");
+//		s1.name = "Shyam"; // this replace the above declared value "Ram" with "Shyam"
+//		s1.age = 35;
+		// Copying s1 into s2
+		CopyConstructor s2 = new CopyConstructor(s1);
+		// if we assign the value of name and age taking s2 object as reference, we can
+		// change the value of name and age only in the copied constructor without
+		// affecting the original constructor's value
+		// example :
+		s2.name = "Hari";
+		s2.faculty = "Management";
+//		s2.age = 22;
+		s1.display();
+		s2.display();
+		
+		//======Exception Case===========
+		/* This is not a copy constructor
+		 * s3 refers to same object as s1
+		 * CopyConstructor s3 = s1;
+		 */
+	}
+}
