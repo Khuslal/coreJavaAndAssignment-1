4f243f107aeb8b2ce9944df8d32073e48ccdd8b5
Khuslal Gupta <guptakhuslal@gmail.com>
2026-05-08 10:37:55 +0545

    Perimeter and Area of Triangle with units 3,4, and 5. respectively.

---
+++ corejava/src/triangle/Triangle.java
@@ -0,0 +1,30 @@
+package triangle;
+
+public class Triangle {
+	double a, b, c;
+
+	Triangle() {
+		a = 3;
+		b = 4;
+		c = 5;
+	}
+
+	public double getPerimeter() {
+		return a + b + c;
+	}
+
+	public double getArea() {
+		double s = getPerimeter() / 2; // s = semi-perimeter;
+		return Math.sqrt(s * (s - a) * (s - b) * (s - c));
+	}
+
+	public static void main(String[] args) {
+		Triangle t = new Triangle();
+		
+		double p = t.getPerimeter(); // p = perimeter;
+		System.out.println("Perimeter : " + p);
+		
+		double a = t.getArea(); // a = area;
+		System.out.println("Area : " + a);
+	}
+}
