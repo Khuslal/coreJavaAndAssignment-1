acee8e9fb49e05a780ddf374d670b479d8b5ab35
Khuslal Gupta <guptakhuslal@gmail.com>
2026-05-08 13:57:36 +0545

    class object practice level 1 qno 6 "Rectange" qno 7 "Area1"

---
+++ corejava/src/areaofrectangle/Area1.java
@@ -0,0 +1,27 @@
+package areaofrectangle;
+
+import java.util.Scanner;
+
+public class Area1 {
+	int length;
+	int breadth;
+	Area1(int l, int b){
+		length = l;
+		breadth = b;
+	}
+	
+	public int returnArea() {
+		return length*breadth;
+	}
+	
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Enter Length");
+		int l = scn.nextInt();
+		System.out.println("Enter Breadth");
+		int b = scn.nextInt();
+		Area1 a1 = new Area1 (l, b);
+		
+		System.out.println("Area : "+a1.returnArea());
+	}
+}
