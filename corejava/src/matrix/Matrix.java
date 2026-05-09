3c0d2e4194093690651063de0788bc30e2d51500
Khuslal Gupta <guptakhuslal@gmail.com>
2026-05-09 23:35:27 +0545

    All the methods are implemented except adding two matrices

--- corejava/src/matrix/Matrix.java
+++ corejava/src/matrix/Matrix.java
@@ -5,60 +5,74 @@ import java.util.Scanner;
 public class Matrix {
 	int noOfRows;
 	int noOfColumns;
-	int elements [][];
-	
+	int elements[][];
+
 	// Constructor
-	Matrix(int noOfRows, int noOfColumns){
+	Matrix(int noOfRows, int noOfColumns) {
 		this.noOfRows = noOfRows;
 		this.noOfColumns = noOfColumns;
 		// Initialize 2D array
 		elements = new int[noOfRows][noOfColumns];
 	}
-	
+
 	// Method
-	public int elements() {
-		return noOfRows*noOfColumns;
+	public int totalElements() {
+		return noOfRows * noOfColumns;
 	}
-	
+
+	public int getRows() {
+		return noOfRows;
+	}
+
+	public int getColumns() {
+		return noOfColumns;
+	}
+
+	// added setElement method for the replacement of 'm1.elements[i][j] =
+	// scn.nextInt();' which was called inside the main method's Matrix object
+	// reference m1 inside the nested loop
+	public void setElement(int i, int j, int value) {
+		elements[i][j] = value;
+	}
+
 	// Main Method
 	public static void main(String[] args) {
-		
+
 		// Scanner and Input
 		Scanner scn = new Scanner(System.in);
 		System.out.println("Enter rows : ");
 		int rows = scn.nextInt();
-		
+
 		System.out.println("Enter columns : ");
 		int columns = scn.nextInt();
-		
-		
+
 		// Creating Object
 		Matrix m1 = new Matrix(rows, columns);
 
 		// Output
-		System.out.println("Rows :"+m1.noOfRows);
-		System.out.println("Columns :"+m1.noOfColumns);
-		System.out.println("Elements :"+m1.elements());
-		
+		System.out.println("Rows :" + m1.getRows());
+		System.out.println("Columns :" + m1.getColumns());
+		System.out.println("Elements :" + m1.totalElements());
+
 		// Looping Statement
-		for(int i = 0; i < rows; i++) {
-			System.out.println("Elements for row "+i+" : ");
-			for(int j = 0; j < columns; j++) {
-				m1.elements[i][j]= scn.nextInt(); 
+		for (int i = 0; i < rows; i++) {
+			System.out.println("Elements for row " + i + " : ");
+			for (int j = 0; j < columns; j++) {
+				// m1.elements[i][j] = scn.nextInt(); // setElement method is created for the replacement of this line
+				m1.setElement(i, j, scn.nextInt());
 			}
 			System.out.println();
 		}
-		
+
 		// For each to retrieve the value
-		for(int row [] : m1.elements) {
+		for (int row[] : m1.elements) {
 			System.out.print("|");
-			for(int element : row) {
-				System.out.print(element+" ");
+			for (int element : row) {
+				System.out.print(element + " ");
 			}
 			System.out.println("|");
 		}
 		scn.close();
 	}
-	
 
 }
