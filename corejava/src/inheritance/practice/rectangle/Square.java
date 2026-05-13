ebb633e3870996c98576a47182f978255a6b0592
Khuslal Gupta <guptakhuslal@gmail.com>
2026-05-13 22:00:30 +0545

    Checking the renamed repo working

--- corejava/src/inheritance/practice/rectangle/Square.java
+++ corejava/src/inheritance/practice/rectangle/Square.java
@@ -1,7 +1,7 @@
 package inheritance.practice.rectangle;
 
 public class Square extends Rectangle {
-
+	
 	Square(int s) {
 // passing 's' as argument for both length and breadth inside the Rectangle Constructor
 		super(s, s);
