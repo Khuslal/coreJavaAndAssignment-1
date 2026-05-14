b0b91f2b145651e53ed556a554a4ad477c0106d2
Khuslal Gupta <guptakhuslal@gmail.com>
2026-05-14 22:21:49 +0545

    Printing area of 10 squares using array of objects

--- corejava/src/inheritance/practice/rectangle/Main.java
+++ corejava/src/inheritance/practice/rectangle/Main.java
@@ -1,5 +1,7 @@
 package inheritance.practice.rectangle;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
 //		Rectangle rec = new Rectangle();
@@ -7,15 +9,30 @@ public class Main {
 //		rec.printPerimeter();
 //		rec.printArea();
 
-		Rectangle rec1 = new Rectangle(4, 5);
+		Rectangle rec1 = new Rectangle(3, 5);
 		rec1.printPerimeter();
 		rec1.printArea();
+		System.out.println();
 
 // passing 3 as argument for param s which assign to length and breadth of parent Rectangle
-		Square sq = new Square(3);
-		sq.printPerimeter();
-		sq.printArea();
-		
-		
+//		Square sq = new Square(3);
+//		sq.printPerimeter();
+//		sq.printArea();
+		Scanner scn = new Scanner(System.in);
+		Square square[] = new Square[10];
+		int value = 0;
+		for (int i = 0; i < square.length; i++) {
+			System.out.println("Enter side for square: ");
+			value = scn.nextInt();
+			Square sq = new Square(value);
+			square[i] = sq;
+		}
+
+		for (Square x : square) {
+			x.printPerimeter();
+			x.printArea();
+		}
+
+		scn.close();
 	}
 }
